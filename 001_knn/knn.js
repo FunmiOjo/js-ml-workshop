e60de3f0605bbccea00f5841cf68c11f9b99f841
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-08-11 17:20:50 -0400

    Wrote _distances method

--- 001_knn/knn.js
+++ 001_knn/knn.js
@@ -1,12 +1,31 @@
 
 //Start off with what passes the first test.
 function KNN(kSize){
-	this.kSize = kSize;
-	this.points = [];
+  this.kSize = kSize;
+  this.points = [];
 }
 
 KNN.prototype.train = function(points) {
-  this.points = this.points.concat(points);
+  this.points = this.points.concat(points)
+}
+
+function subtractVectors(vector1, vector2) {
+  let difference =  vector1.map(function(elem, index) {
+    return vector1[index] - vector2[index]
+  })
+  return difference
+}
+
+function euclideanNorm(vector) {
+  let norm = Math.sqrt(vector.reduce(function(accum, current) {
+    //console.log("accum + (current * current)", accum + (current * current));
+    return accum + (current * current)
+  }, 0))
+  return norm
+}
+
+KNN.prototype._distance = function(vector1, vector2) {
+  return euclideanNorm(subtractVectors(vector1, vector2))
 }
 
 
